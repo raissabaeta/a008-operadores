3245e08c305164a1441653d27edcf6e1d2fd8865
Raissa Baeta <rbaetadev@gmail.com>
2022-08-30 16:56:13 -0300

    Exercicios 1, 2 e 3 prontos

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let numeroPar = Number(prompt("Digite um número par"));
+console.log(numeroPar % 2);
+
+//o resto é sempre 0 quando o número é par;
+//se o usuário digitar um número ímpar o resto será 1;
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let primeiraOperacao = 5 > 20 && 5 < 2;
+let segundaOperacao = 5 === 5 || 5 === "5";
+let terceiraOperacao = !(20 > 50);
+let quartaOperacao = !(20 > 50 || 50 > 60);
+
+console.log(primeiraOperacao);
+console.log(segundaOperacao);
+console.log(terceiraOperacao);
+console.log(quartaOperacao);
+
